af8ccd4ed89e9f72b98f6022eae7d5be1bc7ad57
David Kim <davidkim052894@gmail.com>
2023-05-03 05:52:58 -0500

    STORY-24: header in dashboard

--- src/_metronic/layout/components/header/header-menus/MenuInner.tsx
+++ src/_metronic/layout/components/header/header-menus/MenuInner.tsx
@@ -1,18 +1,19 @@
-import { useIntl } from 'react-intl'
-import { MenuItem } from './MenuItem'
-import { Button, FormControl, Grid, InputLabel, Select, TextField, styled, Typography } from '@mui/material'
-import Calendar from '@mui/icons-material/Event';
-import SearchIcon from '@mui/icons-material/Search';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import SearchIcon from '@mui/icons-material/Search'
+import {Button, Grid, Select, TextField, styled} from '@mui/material'
+import {AdapterDayjs} from '@mui/x-date-pickers/AdapterDayjs'
+import {DatePicker} from '@mui/x-date-pickers/DatePicker'
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider'
+import {useIntl} from 'react-intl'
+import {MenuInnerWithSub} from './MenuInnerWithSub'
+import {MenuItem} from './MenuItem'
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive'
+import {useParams} from 'react-router-dom'
 
 const SearchText = styled(TextField)({
-   '& .MuiInputBase-root': {
+  '& .MuiInputBase-root': {
     backgroundColor: '#F9F9F9',
     borderRadius: 8,
-    height:50,
+    height: 50,
     '&.Mui-focused': {
       backgroundColor: '#fff',
       borderColor: 'grey',
@@ -25,50 +26,62 @@ const SearchText = styled(TextField)({
 })
 
 export function MenuInner() {
-  const intl = useIntl();
-  
+  const intl = useIntl()
+  const params = useParams()
+
   return (
     <>
-      <div style={{padding:'12px'}}>
-        <Grid container flexDirection="row">
+      <div style={{padding: '12px'}}>
+        <Grid container flexDirection='row'>
           {/* <Button >Dashboard</Button>
           <Button >Account</Button> */}
           <MenuItem title={intl.formatMessage({id: 'MENU.DASHBOARD'})} to='/dashboard' />
           <MenuItem title='Account' to='/builder' />
         </Grid>
-        <Grid container columnSpacing={4}>
-          <Grid item>
-            <SearchText placeholder='Search Posts' InputProps={{startAdornment:<SearchIcon/>}}/>
-          </Grid>
-          <Grid item>
+        {params['*'] == 'dashboard' && (
+          <Grid container columnSpacing={4} sx={{mt: 1}}>
+            <Grid item>
+              <SearchText
+                placeholder='Search Posts'
+                InputProps={{startAdornment: <SearchIcon />}}
+              />
+            </Grid>
+            <Grid item>
               <label>Sort By:</label>
-            <Select
-              sx={{ml:1}}
-                labelId="demo-select-small-label"
-                id="demo-select-small"
-                value="latest"
+              <Select
+                sx={{ml: 1}}
+                labelId='demo-select-small-label'
+                id='demo-select-small'
+                value='latest'
                 // value={age}
                 // label="Age"
                 // onChange={handleChange}
               >
-                
                 <option value='latest'>Latest</option>
                 <option value={20}>Twenty</option>
                 <option value={30}>Thirty</option>
               </Select>
-          </Grid>
-          <Grid item>
-            <Grid container alignItems="center">
-            <label>Active Posts between:</label>
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              {/* <DemoContainer components={['DatePicker']}> */}
-                <DatePicker sx={{ml:1}} />
-                <DatePicker sx={{ml:1}} />
-              {/* </DemoContainer> */}
-            </LocalizationProvider>
+            </Grid>
+            <Grid item>
+              <Grid container alignItems='center'>
+                <label>Select Posts:</label>
+                <Select
+                  sx={{ml: 1}}
+                  labelId='demo-select-small-label'
+                  id='demo-select-small'
+                  value='Today'
+                  // value={age}
+                  // label="Age"
+                  // onChange={handleChange}
+                >
+                  <option value='Today'>Today</option>
+                  <option value={20}>Twenty</option>
+                  <option value={30}>Thirty</option>
+                </Select>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
+        )}
       </div>
     </>
   )
