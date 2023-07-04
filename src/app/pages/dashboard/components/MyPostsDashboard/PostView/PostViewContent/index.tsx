4d9415134b1cc1b1d045f975f21df3798431036a
Jesse Schaaf <jesse@threesdesign.net>
2023-07-03 20:40:18 -0500

    do not show map for comments.

--- src/app/pages/dashboard/components/MyPostsDashboard/PostView/PostViewContent/index.tsx
+++ src/app/pages/dashboard/components/MyPostsDashboard/PostView/PostViewContent/index.tsx
@@ -9,41 +9,46 @@ type Props = {
 
 function PostViewContent(props: Props) {
   const {post, type} = props
-  return (
-    <CardContent>
-      <Grid container alignItems='center' sx={{mb: 1}}>
-        <LocationOnIcon color='primary' sx={{mr: 2}} />
-        {post?.address ? (
-          <Typography
-            color='purple'
-            component='a'
-            target='_blank'
-            href={`https://www.google.com/maps/place/${post?.address || ''} ${post?.city || ''} ${
-              post?.state || ''
-            } ${post?.zip_code || ''}`}
-          >
-            {post?.address} , {post?.city} {post?.state}
-          </Typography>
-        ) : (
-          <Typography color='purple'>
-            lat: {post?.lat} lng: {post?.lng}
-          </Typography>
-        )}
-      </Grid>
-      {type === 'post' && (
-        <Grid container alignItems='center' gap={2}>
-          {post?.keyword != 'null' &&
-            JSON.parse(post?.keyword ? post?.keyword : '[]').length > 0 && (
-              <Typography variant='caption'>Keyword: </Typography>
-            )}
-          {post?.keyword != 'null' &&
-            JSON.parse(post?.keyword ? post?.keyword : '[]').map((item: string, index: number) => (
-              <Chip label={item} key={index} />
-            ))}
+  if (type !== 'comment') {
+    return (
+      <CardContent>
+        <Grid container alignItems='center' sx={{mb: 1}}>
+          <LocationOnIcon color='primary' sx={{mr: 2}} />
+          {post?.address ? (
+            <Typography
+              color='purple'
+              component='a'
+              target='_blank'
+              href={`https://www.google.com/maps/place/${post?.address || ''} ${post?.city || ''} ${
+                post?.state || ''
+              } ${post?.zip_code || ''}`}
+            >
+              {post?.address} , {post?.city} {post?.state}
+            </Typography>
+          ) : (
+            <Typography color='purple'>
+              lat: {post?.lat} lng: {post?.lng}
+            </Typography>
+          )}
         </Grid>
-      )}
-    </CardContent>
-  )
+        {type === 'post' && (
+          <Grid container alignItems='center' gap={2}>
+            {post?.keyword != 'null' &&
+              JSON.parse(post?.keyword ? post?.keyword : '[]').length > 0 && (
+                <Typography variant='caption'>Keyword: </Typography>
+              )}
+            {post?.keyword != 'null' &&
+              JSON.parse(post?.keyword ? post?.keyword : '[]').map(
+                (item: string, index: number) => <Chip label={item} key={index} />
+              )}
+          </Grid>
+        )}
+      </CardContent>
+    )
+  }
+  else {
+    return null; // or return an empty fragment: <></>
+  }
 }
 
 export default PostViewContent
