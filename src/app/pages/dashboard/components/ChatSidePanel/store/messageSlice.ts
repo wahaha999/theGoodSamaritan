ca0b8e3d64a0d35f578b41b965cd077d5530bfe6
David Kim <davidkim052894@gmail.com>
2023-07-13 08:36:35 -0400

    STORY-114: exist chatroom added messages

--- src/app/pages/dashboard/components/ChatSidePanel/store/messageSlice.ts
+++ src/app/pages/dashboard/components/ChatSidePanel/store/messageSlice.ts
@@ -8,13 +8,12 @@ export interface IMessage {
   receiver: number
 }
 
-const initialState: any = {messages:[],typeEvent:null}
+const initialState: any = {messages: [], typeEvent: null}
 
 export const sendMessage = createAsyncThunk(
   'dashboard/chat/sendMessage',
   async (data: any, {getState, dispatch}) => {
     const res = await axios.post(`${API_URL}/messages`, data)
-    console.log('message===', res)
   }
 )
 
@@ -30,20 +29,18 @@ export const dmSelect = createAsyncThunk(
     const res = await axios.get(`${API_URL}/getMessages/${channel_id}`)
     dispatch(getMessages(res.data))
     window.Echo.join(`chat.dm.${channel_id}`)
-    .here((users:any) => {
-    }).joining((user:any) => {
-    })
-    .leaving((user:any )=> {
-
-      const message = {
+      .here((users: any) => {})
+      .joining((user: any) => {})
+      .leaving((user: any) => {
+        const message = {
           user: user,
-          message: "Left",
-          status: true
-      };
-      // if (selectedChannelInState.type === "channel") {
-      //     dispatch({ type: ADD_MESSAGE, payload: message });
-      // }
-  })
+          message: 'Left',
+          status: true,
+        }
+        // if (selectedChannelInState.type === "channel") {
+        //     dispatch({ type: ADD_MESSAGE, payload: message });
+        // }
+      })
       .listen('MessageSent', (event: any) => {
         const typingEvent = {
           user: event.user,
@@ -79,20 +76,21 @@ const messageSlice = createSlice({
   initialState,
   reducers: {
     getMessages: (state, action) => {
-      console.log('message=',action.payload)
+      console.log('message=', action.payload)
       return {
         ...state,
-        messages:  action.payload,
-      };    },
+        messages: action.payload,
+      }
+    },
     addMessage: (state, action) => {
       state.messages.push(action.payload)
     },
-    addTypingEvent:(state,action) => {
+    addTypingEvent: (state, action) => {
       state.typeEvent = {...action.payload}
     },
-    removeTypingEvent:(state) => {
+    removeTypingEvent: (state) => {
       state.typeEvent = null
-    }
+    },
   },
   // extraReducers(builder) {
   //   builder.addCase(dmSelect.fulfilled, (state, action) => {
@@ -101,6 +99,6 @@ const messageSlice = createSlice({
   // },
 })
 
-export const {getMessages, addMessage,addTypingEvent,removeTypingEvent} = messageSlice.actions
+export const {getMessages, addMessage, addTypingEvent, removeTypingEvent} = messageSlice.actions
 
 export default messageSlice.reducer
