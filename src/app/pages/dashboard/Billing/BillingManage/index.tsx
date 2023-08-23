b7f09d58fb6d5acd6b68f4890af130f246f5b7ce
David Kim <davidkim052894@gmail.com>
2023-08-23 11:56:07 -0400

    STORY-60: state control function

--- src/app/pages/dashboard/Billing/BillingManage/index.tsx
+++ src/app/pages/dashboard/Billing/BillingManage/index.tsx
@@ -1,17 +1,16 @@
-import {Button} from '@mui/material'
+import {Button, ButtonProps} from '@mui/material'
 import axios from 'axios'
 import React from 'react'
 import {API_URL} from 'src/app/modules/auth/core/_requests'
 
-type Props = {
+type BillingManageProps = {
   customer_id: string
-  color: 'primary' | 'secondary' | 'error'
   title: string
-  variant: 'contained' | 'outlined'
 }
+type ExtendedButtonProps = BillingManageProps & ButtonProps
 
-const BillingManage = (props: Props) => {
-  const {customer_id, color, title, variant} = props
+const BillingManage = (props: ExtendedButtonProps) => {
+  const {customer_id, title} = props
 
   const handleClick = () => {
     axios
@@ -23,9 +22,11 @@ const BillingManage = (props: Props) => {
       .catch((err) => console.log('err=', err))
   }
 
+  // Use ComponentProps to extend Button props
+
   return (
     <>
-      <Button onClick={handleClick} color={color} variant={variant}>
+      <Button onClick={handleClick} {...props}>
         {title}
       </Button>
     </>
